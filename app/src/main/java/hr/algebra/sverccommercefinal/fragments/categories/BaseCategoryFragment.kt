6bf5304afe043a4e54517e8efa8091e5aa89d14a
domagojsliepcevic <dsliepc@racunarstvo.hr>
2023-09-12 18:07:32 +0200

    Base category set up

--- app/src/main/java/hr/algebra/sverccommercefinal/fragments/categories/BaseCategoryFragment.kt
+++ app/src/main/java/hr/algebra/sverccommercefinal/fragments/categories/BaseCategoryFragment.kt
@@ -1,7 +1,69 @@
 package hr.algebra.sverccommercefinal.fragments.categories
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import hr.algebra.sverccommercefinal.R
+import hr.algebra.sverccommercefinal.adapters.BestProductAdapter
+import hr.algebra.sverccommercefinal.databinding.FragmentBaseCategoryBinding
 
-open class BaseCategoryFragment:Fragment(R.layout.fragment_base_category) {
+/**
+ * Base fragment for displaying category-specific products.
+ * All other category fragments inherit from this base fragment.
+ */
+open class BaseCategoryFragment : Fragment(R.layout.fragment_base_category) {
+    private lateinit var binding: FragmentBaseCategoryBinding
+    private lateinit var offerAdapter: BestProductAdapter
+    private lateinit var bestProductAdapter: BestProductAdapter
+
+    /**
+     * Inflates the layout for this fragment and initializes the binding.
+     */
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentBaseCategoryBinding.inflate(inflater)
+        return binding.root
+    }
+
+    /**
+     * Initializes the RecyclerViews for displaying offers and best products.
+     */
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Set up RecyclerView for best products.
+        setupBestProductsRv()
+
+        // Set up RecyclerView for offers.
+        setupOfferRv()
+    }
+
+    /**
+     * Sets up the RecyclerView and its adapter for displaying best products.
+     */
+    private fun setupBestProductsRv() {
+        bestProductAdapter = BestProductAdapter()
+        binding.rvBestProducts.apply {
+            layoutManager = GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
+            adapter = bestProductAdapter
+        }
+    }
+
+    /**
+     * Sets up the RecyclerView and its adapter for displaying offers.
+     */
+    private fun setupOfferRv() {
+        offerAdapter = BestProductAdapter()
+        binding.rvBestProducts.apply {
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = offerAdapter
+        }
+    }
 }
